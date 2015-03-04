b7f598d5f39b3841ab690276ba1ee22e39dc8db7
Tony <tony@raisingthefloor.org>
2015-03-04 11:00:14 +0100

    Added "mail handler" to correctly parse mime-encoded email during tests.

--- tests/js/zombies/zombie-forgot.js
+++ tests/js/zombies/zombie-forgot.js
@@ -10,116 +10,14 @@ var fs         = require("fs");
 
 var isBrowserSane = require("./browser-sanity.js");
 
-require("../test-harness.js");
+require("./zombie-test-harness.js");
 
 var harness = gpii.express.couchuser.tests.harness({});
 
 function runTests() {
     var browser;
 
-    jqUnit.module("End-to-end functional \"forgot password\" tests...", { "setup": function() { browser = Browser.create({ continueOnError: true });  harness.smtp.init(); }});
-
-    jqUnit.asyncTest("Reset a user's password using the \"forgot password\" form...", function() {
-        var timestamp = (new Date()).getTime();
-
-        // Set up a handler to continue the process once we receive an email
-        harness.smtp.applier.change("mailHandler", function(that) {
-            var content = fs.readFileSync(that.model.messageFile);
-
-            // Get the reset code and continue the reset process
-            var resetCodeRegexp = new RegExp("(http.+reset/[a-z0-9-]+)", "i");
-            var matches = content.toString().match(resetCodeRegexp);
-
-            jqUnit.assertNotNull("There should be a reset code in the email sent to the user.", matches);
-            if (matches) {
-                var resetUrl = matches[1];
-                jqUnit.stop();
-
-                // We need a separate browser to avoid clobbering the instance used to generate this email, which still needs to check the results of its activity.
-                var resetBrowser = Browser.create();
-                resetBrowser.visit(resetUrl).then(function(){
-                    jqUnit.start();
-                    isBrowserSane(jqUnit, resetBrowser);
-                    jqUnit.stop();
-
-                    // Fill out the form
-                    resetBrowser.fill("password", timestamp)
-                        .fill("confirm", timestamp)
-                        .pressButton("Reset Password", function() {
-                            jqUnit.start();
-                            isBrowserSane(jqUnit, resetBrowser);
-
-                            // The reset form should no longer be visible
-                            var resetForm = resetBrowser.window.$(".reset-form");
-                            jqUnit.assertNotUndefined("There should be a reset form...", resetForm.html());
-                            jqUnit.assertEquals("The reset form should be hidden...", "none", resetForm.css("display"));
-
-                            // A "success" message should be visible
-                            var feedback = resetBrowser.window.$(".success");
-                            jqUnit.assertNotUndefined("There should be a positive feedback message...", feedback.html());
-
-                            // There should be no alerts
-                            var alert = resetBrowser.window.$(".alert");
-                            jqUnit.assertUndefined("There should not be any alerts...", alert.html());
-
-                            // Log in using the new details
-                            jqUnit.stop();
-                            resetBrowser.visit( harness.express.options.config.express.baseUrl + "content/login").then(function () {
-                                jqUnit.start();
-                                isBrowserSane(jqUnit, resetBrowser);
-                                jqUnit.stop();
-
-                                resetBrowser.fill("username", "reset")
-                                    .fill("password", timestamp)
-                                    .pressButton("Log In", function () {
-                                        jqUnit.start();
-                                        isBrowserSane(jqUnit, resetBrowser);
-
-                                        // The login form should no longer be visible
-                                        var loginForm = resetBrowser.window.$(".login-form");
-                                        jqUnit.assertNotUndefined("There should be a login form...", loginForm.html());
-                                        jqUnit.assertEquals("The login form should be hidden...", "none", loginForm.css("display"));
-
-                                        // A "success" message should be visible
-                                        var feedback = resetBrowser.window.$(".success");
-                                        jqUnit.assertNotUndefined("There should be a positive feedback message...", feedback.html());
-
-                                        // There should be no alerts
-                                        var alert = resetBrowser.window.$(".alert");
-                                        jqUnit.assertUndefined("There should not be any alerts...", alert.html());
-                                    });
-                            });
-                        });
-                });
-            }
-        });
-
-        browser.visit(harness.express.options.config.express.baseUrl + "content/forgot").then(function () {
-            jqUnit.start();
-            isBrowserSane(jqUnit, browser);
-            jqUnit.stop();
-
-            browser
-                .fill("email", "reset@localhost")
-                .pressButton("Send Email", function() {
-                    jqUnit.start();
-                    isBrowserSane(jqUnit, browser);
-
-                    // The "forgot password" form should not be visible
-                    var forgotForm = browser.window.$(".forgot-form");
-                    jqUnit.assertNotUndefined("There should be a \"forgot password\" form...", forgotForm.html());
-                    jqUnit.assertEquals("The \"forgot password\" form should be hidden...", "none", forgotForm.css("display"));
-
-                    // A "success" message should be visible
-                    var feedback = browser.window.$(".success");
-                    jqUnit.assertNotUndefined("There should be a positive feedback message...", feedback.html());
-
-                    // There should be no alerts
-                    var alert = browser.window.$(".alert");
-                    jqUnit.assertUndefined("There should not be an alert...", alert.html());
-                });
-        });
-    });
+    jqUnit.module("End-to-end functional \"forgot password\" tests...", { "setup": function() { browser = Browser.create({ continueOnError: true }); }});
 
     jqUnit.asyncTest("Confirm that passwords must match...", function() {
         var timestamp = (new Date()).getTime();
@@ -233,7 +131,7 @@ function runTests() {
 
     jqUnit.asyncTest("Try to use an invalid reset code...", function() {
         var timestamp = (new Date()).getTime();
-        browser.visit(harness.express.options.config.express.baseUrl + "content/reset/" + timestamp).then(function () {
+        browser.visit(harness.express.options.config.express.baseUrl + "content/reset?code=" + timestamp).then(function () {
             jqUnit.start();
             isBrowserSane(jqUnit, browser);
             jqUnit.stop();
@@ -261,10 +159,124 @@ function runTests() {
         });
     });
 
-    jqUnit.onAllTestsDone.addListener(function() {
-        harness.stop();
+    // Save the test that uses a mail handler for the last.
+    //
+    // If we have to add even one more test that works with email, we will need to refactor to use a testEnvironment
+    // and testCases, as we cannot reuse a mail handler between tests.
+    jqUnit.asyncTest("Reset a user's password using the \"forgot password\" form...", function() {
+        var timestamp = (new Date()).getTime();
+
+        // Set up a handler to continue the process once we receive an email
+        harness.smtp.events.messageReceived.addListener(function(that) {
+            var content = fs.readFileSync(that.options.messageFile);
+
+            // This is a MIME message, it will mangle the lines and special characters unless we decode it.
+            var MailParser = require("mailparser").MailParser,
+                mailparser = new MailParser();
+
+            // If this ends up going any deeper, we will need to refactor using a testEnvironment and testCases.
+            mailparser.on("end", function(mailObject){
+                var content = mailObject.text;
+
+                // Get the reset code and continue the reset process
+                var resetCodeRegexp = new RegExp("(http.+reset[?]code=[a-z0-9-]+)", "i");
+                var matches = content.toString().match(resetCodeRegexp);
+
+                jqUnit.assertNotNull("There should be a reset code in the email sent to the user.", matches);
+                if (matches) {
+                    var resetUrl = matches[1];
+                    jqUnit.stop();
+
+                    // We need a separate browser to avoid clobbering the instance used to generate this email, which still needs to check the results of its activity.
+                    var resetBrowser = Browser.create();
+                    resetBrowser.visit(resetUrl).then(function(){
+                        jqUnit.start();
+                        isBrowserSane(jqUnit, resetBrowser);
+                        jqUnit.stop();
+
+                        // Fill out the form
+                        resetBrowser.fill("password", timestamp)
+                            .fill("confirm", timestamp)
+                            .pressButton("Reset Password", function() {
+                                jqUnit.start();
+                                isBrowserSane(jqUnit, resetBrowser);
+
+                                // The reset form should no longer be visible
+                                var resetForm = resetBrowser.window.$(".reset-form");
+                                jqUnit.assertNotUndefined("There should be a reset form...", resetForm.html());
+                                jqUnit.assertEquals("The reset form should be hidden...", "none", resetForm.css("display"));
+
+                                // A "success" message should be visible
+                                var feedback = resetBrowser.window.$(".success");
+                                jqUnit.assertNotUndefined("There should be a positive feedback message...", feedback.html());
+
+                                // There should be no alerts
+                                var alert = resetBrowser.window.$(".alert");
+                                jqUnit.assertUndefined("There should not be any alerts...", alert.html());
+
+                                // Log in using the new details
+                                jqUnit.stop();
+                                resetBrowser.visit( harness.express.options.config.express.baseUrl + "content/login").then(function () {
+                                    jqUnit.start();
+                                    isBrowserSane(jqUnit, resetBrowser);
+                                    jqUnit.stop();
+
+                                    resetBrowser.fill("username", "reset")
+                                        .fill("password", timestamp)
+                                        .pressButton("Log In", function () {
+                                            jqUnit.start();
+                                            isBrowserSane(jqUnit, resetBrowser);
+
+                                            // The login form should no longer be visible
+                                            var loginForm = resetBrowser.window.$(".login-form");
+                                            jqUnit.assertNotUndefined("There should be a login form...", loginForm.html());
+                                            jqUnit.assertEquals("The login form should be hidden...", "none", loginForm.css("display"));
+
+                                            // A "success" message should be visible
+                                            var feedback = resetBrowser.window.$(".success");
+                                            jqUnit.assertNotUndefined("There should be a positive feedback message...", feedback.html());
+
+                                            // There should be no alerts
+                                            var alert = resetBrowser.window.$(".alert");
+                                            jqUnit.assertUndefined("There should not be any alerts...", alert.html());
+                                        });
+                                });
+                            });
+                    });
+                }
+            });
+
+            // send the email source to the parser
+            mailparser.write(content);
+            mailparser.end();
+        });
+
+        browser.visit(harness.express.options.config.express.baseUrl + "content/forgot").then(function () {
+            jqUnit.start();
+            isBrowserSane(jqUnit, browser);
+            jqUnit.stop();
+
+            browser
+                .fill("email", "reset@localhost")
+                .pressButton("Send Email", function() {
+                    jqUnit.start();
+                    isBrowserSane(jqUnit, browser);
+
+                    // The "forgot password" form should not be visible
+                    var forgotForm = browser.window.$(".forgot-form");
+                    jqUnit.assertNotUndefined("There should be a \"forgot password\" form...", forgotForm.html());
+                    jqUnit.assertEquals("The \"forgot password\" form should be hidden...", "none", forgotForm.css("display"));
+
+                    // A "success" message should be visible
+                    var feedback = browser.window.$(".success");
+                    jqUnit.assertNotUndefined("There should be a positive feedback message...", feedback.html());
+
+                    // There should be no alerts
+                    var alert = browser.window.$(".alert");
+                    jqUnit.assertUndefined("There should not be an alert...", alert.html());
+                });
+        });
     });
 }
 
-// Launch all servers and then start the tests above.
-harness.start(runTests);
+runTests();
