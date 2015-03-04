7b327f63e19bdfa1ec3a9fe3aa540576ff6cbb31
Tony <tony@raisingthefloor.org>
2015-03-04 11:54:20 +0100

    Added end-to-end tests of password reset function.

--- tests/js/server-test-caseholder.js
+++ tests/js/server-test-caseholder.js
@@ -53,6 +53,12 @@ gpii.express.couchuser.test.server.caseHolder.generateUser = function () {
     };
 };
 
+// An expander to generate a new password so that we can confirm that the password reset function actually works more than once.
+gpii.express.couchuser.test.server.caseHolder.generatePassword = function () {
+    var timestamp = (new Date()).getTime();
+    return "password-" + timestamp;
+};
+
 // Each test has a function that is called when its request is issued.
 gpii.express.couchuser.test.server.caseHolder.verifyLoggedIn = function(response, body) {
     gpii.express.couchuser.test.server.caseHolder.isSaneResponse(response, body);
@@ -142,17 +148,30 @@ gpii.express.couchuser.test.server.caseHolder.fullSignupVerifyInitialResponse = 
 // Listen for the email with the verification code and launch the verification request
 gpii.express.couchuser.test.server.caseHolder.fullSignupVerifyEmail = function(signupRequest, verificationRequest, testEnvironment) {
     var content = fs.readFileSync(testEnvironment.smtp.mailServer.options.messageFile);
-    var verificationCodeRegexp = new RegExp("content/verify?code=([a-z0-9-]+)", "i");
-    var matches = content.toString().match(verificationCodeRegexp);
 
-    jqUnit.assertNotNull("There should be a verification code in the email sent to the user.", matches);
+    var MailParser = require("mailparser").MailParser,
+        mailparser = new MailParser();
 
-    signupRequest.code = matches[1];
-    var path = "/api/user/verify?code=" + signupRequest.code;
+    // If this gets any deeper, refactor to use a separate function
+    mailparser.on("end", function(mailObject){
+        var content = mailObject.text;
+        var verificationCodeRegexp = new RegExp("content/verify[?]code=([a-z0-9-]+)", "i");
+        var matches = content.toString().match(verificationCodeRegexp);
 
-    // I can't fix this with the model, so I have to override it completely
-    verificationRequest.options.path = path;
-    verificationRequest.send();
+        jqUnit.assertNotNull("There should be a verification code in the email sent to the user.", matches);
+
+        if (matches) {
+            signupRequest.code = matches[1];
+            var path = "/api/user/verify/" + signupRequest.code;
+
+            // I can't fix this with the model, so I have to override it completely
+            verificationRequest.options.path = path;
+            verificationRequest.send();
+        }
+    });
+
+    mailparser.write(content);
+    mailparser.end();
 };
 
 // Listen for the results of hitting the verification link
@@ -164,6 +183,52 @@ gpii.express.couchuser.test.server.caseHolder.fullSignupVerifyVerificationLink =
 
 // Listen for the results of logging in with our verified acount
 gpii.express.couchuser.test.server.caseHolder.fullSignupVerifyLogin = function(signupRequest, response, body) {
+    gpii.express.couchuser.test.server.caseHolder.isSaneResponse(response, body, 200);
+
+    var data = typeof body === "string" ? JSON.parse(body) : body;
+    jqUnit.assertTrue("The response should be 'ok'.", data.ok);
+    jqUnit.assertNotUndefined("There should be a user returned.", data.user);
+};
+
+// Verify that a password reset request is successful
+gpii.express.couchuser.test.server.caseHolder.fullResetVerifyInitialResponse = function(resetRequest, response, body) {
+    gpii.express.couchuser.test.server.caseHolder.isSaneResponse(response, body, 200);
+};
+
+// Listen for the email with the verification code and launch the verification request
+gpii.express.couchuser.test.server.caseHolder.fullResetVerifyEmail = function(forgotRequest, resetRequest, testEnvironment) {
+    var content = fs.readFileSync(testEnvironment.smtp.mailServer.options.messageFile);
+
+    var MailParser = require("mailparser").MailParser,
+        mailparser = new MailParser();
+
+    // If this gets any deeper, refactor to use a separate function
+    mailparser.on("end", function(mailObject){
+        var content = mailObject.text;
+        var resetCodeRegexp = new RegExp("content/reset[?]code=([a-z0-9-]+)", "i");
+        var matches = content.toString().match(resetCodeRegexp);
+
+        jqUnit.assertNotNull("There should be a reset code in the email sent to the user.", matches);
+
+        if (matches) {
+            forgotRequest.code = matches[1];
+            resetRequest.send({code: forgotRequest.code, password: forgotRequest.options.user.password });
+        }
+    });
+
+    mailparser.write(content);
+    mailparser.end();
+};
+
+// Listen for the results of hitting the reset link
+gpii.express.couchuser.test.server.caseHolder.fullResetVerifyResetLink = function(response, body) {
+    gpii.express.couchuser.test.server.caseHolder.isSaneResponse(response, body, 200);
+    var data = typeof body === "string" ? JSON.parse(body) : body;
+    jqUnit.assertTrue("The response should be 'ok'.", data.ok);
+};
+
+// Listen for the results of logging in with our verified acount
+gpii.express.couchuser.test.server.caseHolder.fullResetVerifyLogin = function(forgotRequest, response, body) {
     gpii.express.couchuser.test.server.caseHolder.isSaneResponse(response, body, 200);
 
     var data = typeof body === "string" ? JSON.parse(body) : body;
@@ -356,8 +421,56 @@ fluid.defaults("gpii.express.couchuser.test.server.caseHolder", {
                 port: "{testEnvironment}.options.port",
                 method: "POST"
             }
-        }
+        },
+        fullResetForgotRequest: {
+            type: "kettle.test.request.httpCookie",
+            options: {
+                path: {
+                    expander: {
+                        funcName: "gpii.express.couchuser.test.server.caseHolder.assembleUrl",
+                        args:     ["{testEnvironment}.options.baseUrl", "api/user/forgot"]
+                    }
 
+                },
+                user: {
+                    name:     "reset",
+                    password: {
+                        expander: {
+                            funcName: "gpii.express.couchuser.test.server.caseHolder.generatePassword"
+                        }
+                    },
+                    email:    "reset@localhost"
+                },
+                port: "{testEnvironment}.options.port",
+                method: "POST"
+            }
+        },
+        fullResetVerifyResetRequest: {
+            type: "kettle.test.request.httpCookie",
+            options: {
+                path: {
+                    expander: {
+                        funcName: "gpii.express.couchuser.test.server.caseHolder.assembleUrl",
+                        args:     ["{testEnvironment}.options.baseUrl", "api/user/reset"]
+                    }
+                },
+                port: "{testEnvironment}.options.port",
+                method: "POST"
+            }
+        },
+        fullResetLoginRequest: {
+            type: "kettle.test.request.httpCookie",
+            options: {
+                path: {
+                    expander: {
+                        funcName: "gpii.express.couchuser.test.server.caseHolder.assembleUrl",
+                        args:     ["{testEnvironment}.options.baseUrl", "api/user/signin"]
+                    }
+                },
+                port: "{testEnvironment}.options.port",
+                method: "POST"
+            }
+        }
     },
     modules: [
         {
@@ -563,7 +676,7 @@ fluid.defaults("gpii.express.couchuser.test.server.caseHolder", {
                         },
                         {
                             listener: "gpii.express.couchuser.test.server.caseHolder.fullSignupVerifyEmail",
-                            event: "{testEnvironment}.smtp.mailServer.events.messageReceived",
+                            event: "{testEnvironment}.smtp.events.messageReceived",
                             args: ["{fullSignupInitialRequest}", "{fullSignupVerifyVerificationRequest}", "{testEnvironment}"]
                         },
                         {
@@ -579,6 +692,47 @@ fluid.defaults("gpii.express.couchuser.test.server.caseHolder", {
                             listener: "gpii.express.couchuser.test.server.caseHolder.fullSignupVerifyLogin",
                             event: "{fullSignupLoginRequest}.events.onComplete",
                             args: ["{fullSignupInitialRequest}", "{fullSignupLoginRequest}.nativeResponse", "{arguments}.0"]
+                        }
+                    ]
+                },
+                {
+                    name: "Testing resetting a user's password, end-to-end...",
+                    type: "test",
+                    sequence: [
+                        { // This sequence point is required because of a QUnit bug - it defers the start of sequence by 13ms "to avoid any current callbacks" in its words
+                            func: "{testEnvironment}.events.constructServer.fire"
+                        },
+                        {
+                            listener: "fluid.identity",
+                            event: "{testEnvironment}.events.started"
+                        },
+                        {
+                            func: "{fullResetForgotRequest}.send",
+                            args: [ { email: "{fullResetForgotRequest}.options.user.email" } ]
+                        },
+                        //{
+                        //    listener: "gpii.express.couchuser.test.server.caseHolder.fullResetVerifyInitialResponse",
+                        //    event: "{fullResetForgotRequest}.events.onComplete",
+                        //    args: ["{fullResetForgotRequest}", "{fullResetForgotRequest}.nativeResponse", "{arguments}.0"]
+                        //},
+                        {
+                            listener: "gpii.express.couchuser.test.server.caseHolder.fullResetVerifyEmail",
+                            event: "{testEnvironment}.smtp.events.messageReceived",
+                            args: ["{fullResetForgotRequest}", "{fullResetVerifyResetRequest}", "{testEnvironment}"]
+                        },
+                        {
+                            listener: "gpii.express.couchuser.test.server.caseHolder.fullResetVerifyResetLink",
+                            event: "{fullResetVerifyResetRequest}.events.onComplete",
+                            args: ["{fullResetVerifyResetRequest}.nativeResponse", "{arguments}.0"]
+                        },
+                        {
+                            func: "{fullResetLoginRequest}.send",
+                            args: [{ name: "{fullResetForgotRequest}.options.user.name", password: "{fullResetForgotRequest}.options.user.password"}]
+                        },
+                        {
+                            listener: "gpii.express.couchuser.test.server.caseHolder.fullResetVerifyLogin",
+                            event: "{fullResetLoginRequest}.events.onComplete",
+                            args: ["{fullResetLoginRequest}", "{fullResetLoginRequest}.nativeResponse", "{arguments}.0"]
                         }
                     ]
                 }
